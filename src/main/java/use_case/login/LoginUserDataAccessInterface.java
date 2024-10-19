6219e9f32b049a933f9f0beaa9629e7cf45ecfb6
arz-c <areez@chishtie.com>
2024-10-19 17:15:17 -0400

    Completed Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user according to the given username.
+     * @param name the username to look up
+     */
+    void setCurrentUser(String name);
 }
